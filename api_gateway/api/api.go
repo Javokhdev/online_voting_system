17c64820804ca359bb2d42f650d80ec16d5fc760
axadjonovsardorbek <axadjonovsardorbeck@gmail.com>
2024-06-06 18:29:49 +0500

    added swagger

--- api_gateway/api/api.go
+++ api_gateway/api/api.go
@@ -4,19 +4,30 @@ import (
 	"gateway/api/handler"
 
 	"github.com/gin-gonic/gin"
+	_ "gateway/docs"
+	// "./docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @Title Online Voting System Swagger UI
+// @BasePath /
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name role
 
 func NewGin(handler *handler.Handler) *gin.Engine {
 
 	r := gin.Default()
 
-	// r.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	// r.Use(Middleware())
 	r.POST("/election/create", handler.CreateElection)
-	// r.POST("/user/getall", handler.GetAllUser)
-	// r.GET("/user/:id", handler.GetByIdUser)
-	// r.POST("/courier", handler.Delivering)
+	r.GET("/election/getbyid", handler.GetByIdElection)
+	r.GET("/election/getall", handler.GetAllElection)
+	r.PUT("/election/update", handler.UpdateElection)
+	// r.Use(Middleware())
 
 	return r
 }
--- api_gateway/api/handler/election.go
+++ api_gateway/api/handler/election.go
@@ -4,10 +4,23 @@ import (
 	"gateway/genproto/voting"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary Create a new election
+// @Description Endpoint for creating a new election
+// @Name create_election
+// @Date create_election
+// @Tags Election
+// @Accept json
+// @Produce json
+// @Param election body voting.CreateElectionReq true "Election creation request payload"
+// @Success 200 {object} voting.Void "Successfully created election"
+// @Failure 400 {object} string "Invalid request payload"
+// @Failure 500 {object} string "Failed to create election"
+// @Router /election/create [POST]
 func (h *Handler) CreateElection(ctx *gin.Context) {
 	es := voting.CreateElectionReq{}
 
@@ -24,9 +37,99 @@ func (h *Handler) CreateElection(ctx *gin.Context) {
 	if err != nil {
 		slog.Info("error election binding.", "err", err.Error())
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "error in server"})
-		return 
+		return
 	}
 
 	ctx.JSON(200, "Election successfully created")
 }
 
+// @Summary Get Election
+// @Description Endpoint for getting election
+// @Tags Election
+// @Accept json
+// @Produce json
+// @Param  id query voting.ById true "ID"
+// @Success 200 {object} voting.GetElectionRes "Successfully getted election"
+// @Failure 400 {object} string "Invalid request payload"
+// @Failure 500 {object} string "Failed to get election"
+// @Router /election/getbyid [GET]
+func (h *Handler) GetByIdElection(ctx *gin.Context) {
+	id := voting.ById{}
+
+	id.Id = ctx.Query("id")
+
+	election, err := h.srvs.ElectionService.GetById(ctx, &id)
+
+	if err != nil {
+		slog.Info("error election geting.", "err", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "error in server"})
+		return
+	}
+
+	ctx.JSON(200, election)
+}
+
+// @Summary Get All Elections
+// @Description Endpoint for getting all elections
+// @Tags Election
+// @Accept json
+// @Produce json
+// @Param election query voting.Filter true "Elections geting request payload"
+// @Success 200 {object} voting.GetAllElectionRes "Successfully getted elections"
+// @Failure 400 {object} string "Invalid request payload"
+// @Failure 500 {object} string "Failed to get elections"
+// @Router /election/getall [GET]
+func (h *Handler) GetAllElection(ctx *gin.Context) {
+	filter := voting.Filter{}
+
+	limit, _ := strconv.Atoi(ctx.Query("limit"))
+	offset, _ := strconv.Atoi(ctx.Query("offset"))
+
+	filter.Limit = int32(limit)
+	filter.Offset = int32(offset)
+
+	elections, err := h.srvs.ElectionService.GetAll(ctx, &filter)
+
+	if err != nil {
+		slog.Info("error elections getting.", "err", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "error in server"})
+		return
+	}
+
+	ctx.JSON(200, elections)
+}
+
+// @Summary Update election
+// @Description Endpoint for updating election
+// @Id update_election
+// @Name update_election
+// @Date update_election
+// @Tags Election
+// @Accept json
+// @Produce json
+// @Param election body voting.GetElectionRes true "Election updaing request payload"
+// @Success 200 {object} voting.Void "Successfully updated election"
+// @Failure 400 {object} string "Invalid request payload"
+// @Failure 500 {object} string "Failed to update election"
+// @Router /election/update [PUT]
+func (h *Handler) UpdateElection(ctx *gin.Context){
+	election := voting.GetElectionRes{}
+
+	err := ctx.ShouldBindJSON(&election)
+
+	if err != nil {
+		slog.Info("error election binding.", "err", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Not valid JSON"})
+		return
+	}
+
+	_, err = h.srvs.ElectionService.Update(ctx, &election)
+
+	if err != nil {
+		slog.Info("error election binding.", "err", err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "error in server"})
+		return
+	}
+
+	ctx.JSON(200, "Election successfully updated")
+}
