274579f5b0e4fe9d99a991c4510bc708a17d173d
Javohir <javohdev@gmail.com>
2024-06-06 16:17:01 +0500

    api-gateway begins

---
+++ api_gateway/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"gateway/api"
+	"gateway/api/handler"
+	"gateway/config"
+	"gateway/grpc/client"
+	"log"
+)
+
+
+func main() {
+	cfg := config.Load()
+
+	services, err := client.NewGrpcClients(&cfg)
+	if err != nil {
+		log.Fatalf("error while connecting clients. err: %s", err.Error())
+	}
+
+	
+	engine := api.NewGin(handler.NewHandler(services))
+
+	err = engine.Run(cfg.HTTP_PORT)
+	if err != nil {
+		log.Fatalf("error while running server. err: %s", err.Error())
+	}
+}
